ada657b4bf7c7f4e06ceccc52641a594b5db5982
Vinodkumar Borse <borse002@gmail.com>
2024-09-23 09:16:14 +0530

    Add files via upload

--- school-web-app/src/main/java/com/ts/School.java
+++ school-web-app/src/main/java/com/ts/School.java
@@ -13,6 +13,20 @@ public class School extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 	        throws ServletException, IOException
 	    {
+			
+			String name = req.getParameter("q");
+			String result = "";
+			if(name.equals("cricket")) {
+				result = "Get Live Cricket Scores, Ball by Ball Commentary, Scorecard Updates, Match Facts & related News of all the International & Domestic Cricket";
+			} else if(name.equals("movie")) {
+				result = "List of Upcoming Movies in year 2024 and 2025. Watch Movie Trailers, Release Date, Latest News of movies in Mumbai coming soon at theatres near you.";
+			}  else {
+				result = "Sorry";
+			}
+			
 			System.out.println("We are in backend file");
+			
+			req.setAttribute("data", result);
+			req.getRequestDispatcher("school.jsp").forward(req, resp);
 	    }
 }
---
+++ school-web-app/src/main/webapp/school.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=US-ASCII"
+    pageEncoding="US-ASCII" isELIgnored = "false"%>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Day-3</title>
+</head>
+  <body>
+		    <form action="search" method="get">
+		        <label>Search:-</label>
+		        <input type="text" name="q">
+		        <input type="submit" value="click me">
+		    </form>
+    <h1>${data}</h1>
+  </body>
+</html>
